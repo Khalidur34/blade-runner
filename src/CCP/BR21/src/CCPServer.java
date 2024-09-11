c532d28dae723e56cd94128aac7b74163b077132
Kev1ar <kevin.la26@hotmail.com>
2024-09-11 14:42:42 +1000

    CCP Server: adding address field

--- src/CCP/BR21/src/CCPServer.java
+++ src/CCP/BR21/src/CCPServer.java
@@ -1,16 +1,26 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 
 public class CCPServer extends Thread {
 
     private DatagramSocket socket;
+    private InetSocketAddress socketAddress;
+
     private byte[] buf;
     private boolean running;
 
-    public CCPServer(DatagramSocket socket, int messageSize) throws SocketException {
-        this.socket = socket;
+    public CCPServer(int portNumber, String address, int messageSize)
+            throws SocketException, UnknownHostException {
+        // Create socket address from port number and IP,
+        // Create port to listen on socket address,
+        // Set message size
+        this.socketAddress = new InetSocketAddress(InetAddress.getByName(address), portNumber);
+        this.socket = new DatagramSocket(this.socketAddress);
         this.buf = new byte[messageSize];
     }
 
@@ -22,17 +32,18 @@ public class CCPServer extends Thread {
                 // Recieve DatagramPacket and extract JSON payload
                 DatagramPacket packet = new DatagramPacket(buf, buf.length);
                 socket.receive(packet);
-
                 System.out.println("PACKAGE RECIEVED");
 
+                // Extract Payload from packet
                 String jsonPayload = new String(packet.getData(), 0, packet.getLength());
                 System.out.println("Received JSON: " + jsonPayload);
 
                 // Pass JSON payload to Message Reader
                 MessagePraser.readJsonMessage(jsonPayload);
+
             } catch (IOException e) {
                 e.printStackTrace();
-                running = false; // Stop the server if there's an IO exception
+                running = false; // Stop Server
             }
         }
         socket.close();

e00dca673ef0a1ba792d91d1bdca9373990706d2
Kevin <kevin.la26@hotmail.com>
2024-09-11 11:14:32 +1000

    MessagePraser

--- src/CCP/BR21/src/CCPServer.java
+++ src/CCP/BR21/src/CCPServer.java
@@ -3,7 +3,6 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
 
-
 public class CCPServer extends Thread {
 
     private DatagramSocket socket;
@@ -11,23 +10,23 @@ public class CCPServer extends Thread {
     private boolean running;
 
     public CCPServer(DatagramSocket socket, int messageSize) throws SocketException {
-       this.socket = socket;
-       this.buf = new byte[messageSize];
+        this.socket = socket;
+        this.buf = new byte[messageSize];
     }
 
     public void run() {
         running = true;
+        System.out.println("SERVER STARTED");
         while (running) {
             try {
+                // Recieve DatagramPacket and extract JSON payload
                 DatagramPacket packet = new DatagramPacket(buf, buf.length);
                 socket.receive(packet);
                 String jsonPayload = new String(packet.getData(), 0, packet.getLength());
-
                 System.out.println("Received JSON: " + jsonPayload);
 
-                MessagePraser praser = new MessagePraser();
-                praser.readJsonMessage(jsonPayload);
-                socket.send(packet);
+                // Pass JSON payload to Message Reader
+                MessagePraser.readJsonMessage(jsonPayload);
             } catch (IOException e) {
                 e.printStackTrace();
                 running = false; // Stop the server if there's an IO exception

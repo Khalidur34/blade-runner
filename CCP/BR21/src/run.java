28b3be47b478d0edd63298c995e943bba0bbc43f
Kev1ar <kevin.la26@hotmail.com>
2024-09-18 00:07:54 +1000

    Removing src folder

--- CCP/BR21/src/run.java
+++ CCP/BR21/src/run.java
@@ -3,25 +3,23 @@ import java.net.DatagramSocket;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
-
-
 public class run {
     UDPClient client;
-    public static void main (String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World!");
 
-       run a = new run();
-       a.setup();
+        run a = new run();
+        a.setup();
         a.whenCanSendAndReceivePacket_thenCorrect();
     }
 
-    public void setup() throws SocketException, UnknownHostException{
-         CCPServer a = new CCPServer(new DatagramSocket(4302),500);
-         a.start();
+    public void setup() throws SocketException, UnknownHostException {
+        CCPServer a = new CCPServer(4302, "localhost", 1024);
+        a.start();
     }
 
-   
     public void whenCanSendAndReceivePacket_thenCorrect() {
-        SendingData.update("womp",false,0,"BR20","that one");
+        SendingData.update("womp", false, 0, "BR20", "that one");
     }
 }
